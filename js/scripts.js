4840c393c58d7c96d439a81abb56eb0c9def6cd6
Andreas W. Keller <131688400+AKeWeb@users.noreply.github.com>
2023-05-20 14:16:48 +0200

    Creating Pokemon Objects and Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-alert("Hello World");
+// Creating Pokemon Objects:
+let pokemon1 = {
+  name: Butterfree,
+  height: 5,
+  type: [poision, grass],
+};
 
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+let pokemon2 = {
+  name: Weedle,
+  height: 3,
+  type: [rock, fire],
+};
+
+let pokemon3 = {
+  name: Rattata,
+  height: 3,
+  type: [ice, water],
+};
+
+// Including Pokemon Objects into Pokemon List (Array):
+let pokemonList = [pokemon1, pokemon2, pokemon3];
